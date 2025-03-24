39555d56e7a6ca60f47854534344daa35fc50341
jelebe <jesusocu@hotmail.com>
2025-03-24 17:52:53 +0100

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         minSdk = 29
         targetSdk = 35
         versionCode = 1
-        versionName = "1.3.3"
+        versionName = "1.3.4"
 
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
