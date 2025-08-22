2930d5f162d991ad7a5e9c81daa9c9cf22250956
jelebe <48688326+jelebe@users.noreply.github.com>
2025-08-22 16:14:10 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.besos.bpm"
         minSdk = 29
         targetSdk = 35
-        versionCode = 44
-        versionName = "1.4.4"
+        versionCode = 51
+        versionName = "1.5.1"
 
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
